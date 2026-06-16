9a85ded25425a4fc3f22c6ee3397ceeeea91aaf8
Rafael <rafaelnonamer@gmail.com>
2026-06-16 17:13:56 +0200

    Delete lib directory

--- lib/env.ts
+++
@@ -1,1 +0,0 @@
-export const env={NEXT_PUBLIC_APP_URL:process.env.NEXT_PUBLIC_APP_URL||"http://localhost:3000"};
--- lib/format.ts
+++
@@ -1,2 +0,0 @@
-export function money(cents:number,currency="EUR"){return new Intl.NumberFormat("pt-PT",{style:"currency",currency}).format(cents/100)}
-export function cn(...v:Array<string|false|undefined|null>){return v.filter(Boolean).join(" ")}
